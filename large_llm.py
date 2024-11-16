ed9e8d8822e2591f8e6be9c5e286d9f963838da0
Shrivardhan Rao <Shrivardhan_Rao@intuit.com>
2024-11-15 17:24:07 -0800

    offload to cpu

--- large_llm.py
+++ large_llm.py
@@ -116,6 +116,7 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
     engine_args.max_model_len=2000
     engine_args.gpu_memory_utilization=0.94
     engine_args.cpu_offload_gb=5
+    engine_args.lora_dtype="float16"
 
     return VLLMDeployment.bind(
         engine_args,
