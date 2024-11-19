91854123a252b8d302720a93662ce4ca3ecdfe91
Shrivardhan Rao <Shrivardhan_Rao@intuit.com>
2024-11-18 21:56:08 -0800

    update engineargs

--- large_llm.py
+++ large_llm.py
@@ -112,6 +112,7 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
     parsed_args = parse_vllm_args(cli_args)
     engine_args = AsyncEngineArgs.from_cli_args(parsed_args)
     engine_args.worker_use_ray = True
+    engine_args.device="cuda"
 
     return VLLMDeployment.bind(
         engine_args,
