fe6f897149ec2e00edecc6111786716c9151f18f
Shrivardhan Rao <shrivardhan92@gmail.com>
2024-12-03 16:00:27 -0800

    use latest arg

--- llama_instruct.py
+++ llama_instruct.py
@@ -116,7 +116,7 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
     """  # noqa: E501
     parsed_args = parse_vllm_args(cli_args)
     engine_args = AsyncEngineArgs.from_cli_args(parsed_args)
-    engine_args.worker_use_ray = True
+    engine_args.distributed_executor_backend = "ray"
     engine_args.device="cuda"
 
     return VLLMDeployment.bind(
